13ffe017dafc13275cd39c9e9a9303645e993a8b
unknown <jinhan.loh.2021@scis.smu.edu.sg>
2022-07-20 15:01:20 +0800

    Added new folders.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
+import { Header } from "./styles";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
+  return <Header>{children}</Header>;
 }
