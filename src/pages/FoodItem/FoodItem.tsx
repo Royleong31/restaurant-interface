e272deb02a402e36b81b135a3e2fa0919b624509
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2022-08-02 15:44:57 +0800

    First commit on foodItemPage branch.

--- src/pages/FoodItem/FoodItem.tsx
+++ src/pages/FoodItem/FoodItem.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 
 type Props = {};
 
+//New branch first commit
 export default function FoodItem({}: Props) {
   const { foodItemName } = useParams(); //gets the foodItemName from the URL
   return <div>{foodItemName}</div>;
