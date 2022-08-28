12f806b1a07e2fe72cd37ef6dd9f174dba62f6d4
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2022-08-28 23:18:02 +0800

    First commit on Basket Page.

---
+++ src/pages/Basket/Basket.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { BasketStyle } from "./Basket.style";
+
+type Props = {};
+
+export default function Basket({}: Props) {
+  return <BasketStyle>Basket</BasketStyle>;
+}
